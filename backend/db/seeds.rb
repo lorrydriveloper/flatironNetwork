97f4b70fb6e27d0eff3e82d74738bad11141b325
reddevilcero <reddevil_cero@hotmail.com>
2020-05-19 17:42:20 +0100

    companies Resources created

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+companies = [
+  {
+    name: 'Google',
+    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/320px-Google_2015_logo.svg.png',
+    address: '1600 Amphitheatre Parkway Mountain View, CA 94043'
+  }, 
+  {
+    name: 'Flatiron School',
+    logo: 'https://en.wikipedia.org/wiki/Flatiron_School#/media/File:FS_wiki.png',
+    address: '11 Broadway, 2nd Floor New York , NY 10004'
+  }
+]
+
+Company.create(companies)
