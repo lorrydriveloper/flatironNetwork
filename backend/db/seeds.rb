d1f081fd77374585b0e2393002a1c1cb9b71d8f6
reddevilcero <reddevil_cero@hotmail.com>
2020-05-20 15:15:16 +0100

    add a seed complex file

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -7,6 +7,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Company.destroy_all
+User.destroy_all
+
 companies = [
   {
     name: 'Google',
@@ -35,5 +38,30 @@ companies = [
     logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Linkedin_icon.svg/240px-Linkedin_icon.svg.png'
   }
 ]
-Company.destroy_all
 Company.create(companies)
+
+20.times do
+  Company.create(
+    name: Faker::Company.name,
+    logo: Faker::Company.logo
+  )
+end
+
+course = ['Software Engineering', 'Data Science', 'Cybersecurity', 'UX/UI Design']
+campus = ['Austin', 'Chicago', 'Denver', 'Houston', 'New York', 'San Francisco', 'Seattle', 'Washington, D.C.', 'London', 'Online']
+
+Company.all.each do |company|
+  rand(0...20).times do
+    date = Faker::Date.in_date_period(year: rand(2019...2020), month: rand(1..12))
+    User.create(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      avatar: Faker::Avatar.image,
+      cohort: "#{date.month}/#{date.year}",
+      campus: campus[rand(0...campus.size)],
+      course: course[rand(0...course.size)],
+      company: company,
+      work_location: campus[rand(0...campus.size)]
+    )
+  end
+end
