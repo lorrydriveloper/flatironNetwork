7211e29f94f125ff578b64f21f22b258c0866381
reddevilcero <reddevil_cero@hotmail.com>
2020-05-20 13:22:11 +0100

    first fetch to companies

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,40 @@
-console.log('Conneted...')
+const companies = document.querySelector('#companies .grid-container')
+
+document.addEventListener('DOMContentLoaded', function () {
+  fetchCompanies()
+  
+})
+
+function fetchCompanies() {
+  let configurationObject = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    // body: JSON.stringify({ trainer_id: trainerId }),
+  };
+
+  fetch("http://localhost:3000/api/v1/companies", configurationObject)
+    .then((response) => response.json())
+    .then((json) => {
+        companies.innerHTML += json.map(renderCompany).join('')
+    })
+    .catch((error) => console.log(error.message));
+}
+
+function renderCompany({logo, name, slug}) {
+  
+  return `
+          <div class="company">
+            <img
+              src="${logo}"
+              alt="${slug}-logo"
+            />
+            <div class="company__info">
+              <h3>${name}</h3>
+              <h4>grads working here 3</h4>
+            </div>
+          </div>
+  `;
+}
