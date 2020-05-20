5cd5bac1efa04aedb5195fa5db43bd1882b18a54
reddevilcero <reddevil_cero@hotmail.com>
2020-05-20 13:09:13 +0100

    modifed comapnies migration and seed file.

--- backend/db/migrate/20200519143806_create_companies.rb
+++ backend/db/migrate/20200519143806_create_companies.rb
@@ -3,8 +3,6 @@ class CreateCompanies < ActiveRecord::Migration[6.0]
     create_table :companies do |t|
       t.string :name
       t.string :logo
-      t.string :address
-
       t.timestamps
     end
   end
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -8,17 +10,30 @@
 companies = [
   {
     name: 'Google',
-    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/320px-Google_2015_logo.svg.png',
-    address: '1600 Amphitheatre Parkway Mountain View, CA 94043'
-  }, 
+    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/235px-Google_%22G%22_Logo.svg.png'
+  },
   {
     name: 'Flatiron School',
-    logo: 'https://en.wikipedia.org/wiki/Flatiron_School#/media/File:FS_wiki.png',
-    address: '11 Broadway, 2nd Floor New York , NY 10004'
+    logo: 'https://coursereport-s3-production.global.ssl.fastly.net/rich/rich_files/rich_files/999/s200/flatironschool.png'
+  }, {
+    name: 'Spotify',
+    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/240px-Spotify_logo_without_text.svg.png'
   },
-    name: "Spotify USA",
-    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/240px-Spotify_logo_without_text.svg.png",
-    address: "4 World Trade Center 150 Greenwich Street, 62nd Floor New York, NY 10007 USA",
+  {
+    name: 'Facebook',
+    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Facebook_icon.svg/240px-Facebook_icon.svg.png'
+  }, {
+    name: 'IBM',
+    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/IBM_logo.svg/200px-IBM_logo.svg.png'
+  },
+  {
+    name: 'NASA',
+    logo: 'https://upload.wikimedia.org/wikipedia/commons/e/e5/NASA_logo.svg'
+  },
+  {
+    name: 'LinkedIn',
+    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Linkedin_icon.svg/240px-Linkedin_icon.svg.png'
+  }
 ]
 Company.destroy_all
 Company.create(companies)
