aab4f4a41bec4b22ceec04e76f17beadd825a0c5
reddevilcero <reddevil_cero@hotmail.com>
2020-05-21 21:03:44 +0100

    make async call to DB for company and users

--- frontend/modules/api.js
+++ frontend/modules/api.js
@@ -2,8 +2,8 @@ import DOM from "../modules/render.js";
 
 class ApiAdapter {
   static URL = "http://localhost:3000/api/v1/";
-
-  static getRequest(url) {
+  static allGrads = [];
+  static async getRequest(url) {
     let configurationObject = {
       method: "GET",
       headers: {
@@ -11,20 +11,30 @@ class ApiAdapter {
         Accept: "application/json",
       },
     };
-    return fetch(this.URL + url, configurationObject)
-      .then((response) => response.json())
-      .then((json) => json)
-      .catch((error) => console.log(error.message));
+    try {
+      const response = await fetch(this.URL + url, configurationObject);
+      const json = await response.json();
+      return json;
+    }
+    catch (error) {
+      return console.log(error.message);
+    }
   }
 
   static async fetchGrads() {
     let grads = await this.getRequest("users");
-    DOM.displayGrads.innerHTML += grads.map(DOM.renderGrad).join("");
+    this.allGrads = grads
+    DOM.displayGrads.innerHTML += DOM.HTMLify(grads, DOM.renderGrad);
   }
 
   static async fetchCompanies() {
     let companies = await this.getRequest("companies");
-    DOM.displayCompanies.innerHTML += companies.map(DOM.renderCompany).join("");
+    DOM.displayCompanies.innerHTML += DOM.HTMLify(companies, DOM.renderCompany);
+  }
+  static async fetchCompany(id) {
+    let company = await this.getRequest(`companies/${id}`);
+    DOM.displayInfo.innerHTML = "";
+    DOM.displayInfo.innerHTML += DOM.HTMLify(company.users, DOM.renderGrad);
   }
 }
 
