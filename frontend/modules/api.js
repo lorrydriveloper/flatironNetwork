517a497f5c3ab554d1cec907f8b50d2171064333
reddevilcero <reddevil_cero@hotmail.com>
2020-05-22 13:09:46 +0100

    grab all companies in array for search

--- frontend/modules/api.js
+++ frontend/modules/api.js
@@ -4,6 +4,7 @@ import Map from "../modules/map.js";
 class ApiAdapter {
   static URL = "http://localhost:3000/api/v1/";
   static allGrads = [];
+  static allCompanies = [];
   static async getRequest(url) {
     let configurationObject = {
       method: "GET",
@@ -16,26 +17,26 @@ class ApiAdapter {
       const response = await fetch(this.URL + url, configurationObject);
       const json = await response.json();
       return json;
-    }
-    catch (error) {
+    } catch (error) {
       return console.log(error.message);
     }
   }
 
   static async fetchGrads() {
     let grads = await this.getRequest("users");
-    this.allGrads = grads
-    Map.init(grads)
+    this.allGrads = grads;
+    Map.init(grads);
     DOM.displayGrads.innerHTML += DOM.HTMLify(grads, DOM.renderGrad);
   }
 
   static async fetchCompanies() {
     let companies = await this.getRequest("companies");
+    this.companies = companies
     DOM.displayCompanies.innerHTML += DOM.HTMLify(companies, DOM.renderCompany);
   }
   static async fetchCompany(id) {
     let company = await this.getRequest(`companies/${id}`);
-    Map.init(company.users)
+    Map.init(company.users);
     DOM.displayInfo.innerHTML = "";
     DOM.displayInfo.innerHTML += DOM.HTMLify(company.users, DOM.renderGrad);
   }
