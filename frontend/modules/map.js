65c7e2ddbba365eff322bc5a93ab83384c2ccc92
reddevilcero <reddevil_cero@hotmail.com>
2020-05-22 10:48:16 +0100

    add Grad card to map marker via InfoWindows

--- frontend/modules/map.js
+++ frontend/modules/map.js
@@ -1,3 +1,5 @@
+import DOM from "../modules/render.js";
+
 class Map {
   static init(gradsArray) {
     const map = new google.maps.Map(document.getElementById("map"), {
@@ -5,36 +7,10 @@ class Map {
       zoom: 3,
     });
 
-    var contentString = `
-      <div id="content">
-      <div id="siteNotice"></div>
-      <h1 id="firstHeading" class="firstHeading">Uluru</h1>
-      <div id="bodyContent">
-       <p>
-        <b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large sandstone
-        rock formation in the southern part of the Northern Territory, central
-        Australia. It lies 335&#160;km (208&#160;mi) south west of the nearest
-        large town, Alice Springs; 450&#160;km (280&#160;mi) by road. Kata Tjuta
-        and Uluru are the two major features of the Uluru - Kata Tjuta National
-        Park. Uluru is sacred to the Pitjantjatjara and Yankunytjatjara, the
-        Aboriginal people of the area. It has many springs, waterholes, rock caves
-        and ancient paintings. Uluru is listed as a World Heritage Site.
-      </p>
-    <p>
-      Attribution: Uluru,
-      <a
-        href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194"
-      >
-        https://en.wikipedia.org/w/index.php?title=Uluru</a
-      >
-      (last visited June 22, 2009).
-    </p>
-  </div>
-</div>`;
 
-    let markers = gradsArray.map((a) => {
-      var infowindow = new google.maps.InfoWindow({
-        content: contentString,
+    let markers = gradsArray.map((grad) => {
+      const infowindow = new google.maps.InfoWindow({
+        content: DOM.renderGrad(grad),
       });
       let marker = new google.maps.Marker({
         position: {
@@ -42,7 +18,7 @@ class Map {
           lng: Map.getRandomInRange(-180, 180, 7),
         },
         map: map,
-        title: `hello`,
+        title: grad.name,
         infowindow: infowindow,
       });
       marker.addListener("click", function () {
