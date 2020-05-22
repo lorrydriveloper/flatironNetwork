0934d9ef9198a3e612a739da36bb391df844a4b7
reddevilcero <reddevil_cero@hotmail.com>
2020-05-22 19:56:57 +0100

    create user from Ajax call

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -2,8 +2,26 @@
 
 class Api::V1::UsersController < ApplicationController
   def index
-    students = User.all
+    users = User.all
 
-    render json: students, except: %i[created_at updated_at]
+    render json: users, except: %i[created_at updated_at]
+  end
+
+  def create
+    user = User.new(user_params)
+    user.company = Company.friendly.find(params[:user][:company])
+    if user.save
+      render json: user, except: %i[created_at updated_at]
+    else
+      render json: { status: 'error', message: "Can't find " }, status: :bad_request
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :avatar,
+                                 :cohort, :campus, :course, :street,
+                                 :city, :postcode, :state, :country)
   end
 end
