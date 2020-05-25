f0169fafa3176422c1b022460dd8fd28732c9c60
reddevilcero <reddevil_cero@hotmail.com>
2020-05-25 12:22:15 +0100

    added validations for more consistent data

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -4,8 +4,15 @@ class User < ApplicationRecord
   geocoded_by :address
   after_validation :geocode
   belongs_to :company
+  before_save { self.email = email.downcase }
+  validates :email, presence: true, uniqueness: true
+  validates :street, :city, :postcode, :country, presence: true
 
   def address
-    [street, city, postcode, state, country].compact.join(', ')
-end
+    if country == 'United Stated Of America'
+      [street, city, postcode, state, country].compact.join(', ')
+    else
+      [street, city, postcode, country].compact.join(', ')
+    end
+  end
 end
