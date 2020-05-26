eba3e9a330dc7f49cf4db22a02d300178c92916e
reddevilcero <reddevil_cero@hotmail.com>
2020-05-26 12:35:44 +0100

    moving some function to HTMLBuilder

--- frontend/modules/HTMLbuilder.js
+++ frontend/modules/HTMLbuilder.js
@@ -1,18 +1,131 @@
-class HTMLBuilder{
+import ApiAdapter from "../modules/api.js";
 
-
-  static errors(messages){
+class HTMLBuilder {
+  static errors(messages) {
+   let toDisplay =  messages ? messages : 'Oops, An error has ocurred please try again'
     return `
     <div class="errors">
-      <p>${messages}</p>
+      <p>${toDisplay}</p>
     </div>
         
     `;
   }
 
-  
+  static datalist(collection) {
+    return `
+      <datalist id="companies">
+         ${collection
+           .map((c) => `<option value="${c.slug}">${c.name}</option>`)
+           .join("")}
+      </datalist>
+    `;
+  }
 
+  static formBuilder() {
+    let html = '<form action="">';
+    let course = [
+      "Software Engineering",
+      "Data Science",
+      "Cybersecurity",
+      "UX/UI Design",
+    ];
+    let campus = [
+      "Austin",
+      "Chicago",
+      "Denver",
+      "Houston",
+      "New York",
+      "San Francisco",
+      "Seattle",
+      "Washington, D.C.",
+      "London",
+      "Online",
+    ];
+
+    let attributes = [
+      "name",
+      "email",
+      "avatar",
+      "cohort",
+      "course",
+      "campus",
+      "company",
+      "street",
+      "city",
+      "postcode",
+      "state",
+      "country",
+      "remote_work",
+    ];
+    html += attributes
+      .map((attr) => {
+        if (attr == "course") {
+          return this.optionsBuilder(course, attr);
+        } else if (attr == "company") {
+          return this.optionsBuilder(ApiAdapter.companies, attr);
+        } else if (attr == "cohort") {
+          return `
+            <div class="float-label">
+              <input type="month" id="cohort" name="cohort"
+                min="2012-01">
+               <label for="cohort">Select you Cohort:</label>
+            </div>
+            `;
+        } else if (attr == "campus") {
+          return this.optionsBuilder(campus, attr);
+        } else if (attr == "remote_work") {
+          return `
+            <div class="float-label">
+              <input type="radio" id="Remote" name='remote_work' value="true">
+              <label for="Remote">Working Remotely</label><br>
+              <input type="radio" id="Office" name='remote_work' value="false" checked>
+              <label for="Office">Office Base</label><br>
+            </div>
+            `;
+        } else {
+          return `   
+       <div class="float-label">
+          <input type="text" name="${attr}" required />
+            <label for="${attr}">${attr}</label>
+        </div>
+            `;
+        }
+      })
+      .join("");
+    html +=
+      '</form><input type="submit" value="submit" class="button__add button__submit">';
+    return html;
+  }
+
+  static optionsBuilder(collection, attr) {
+    return `     
+    <div class="float-label">     
+    <select name="${attr}" id="${attr} required">
+      <option>select you ${attr}</option>
+      ${collection
+        .map((c) => {
+          let option = "";
+          if (typeof c == "object") {
+            option = `<option value="${c.slug}">${c.name}</option>`;
+          } else {
+            option = `<option value="${c}">${c}</option>`;
+          }
+          return option;
+        })
+        .join("")}
+    </select>
+    </div>`;
+  }
 }
 
 
 export default HTMLBuilder
+
+
+// <datalist id="ice-cream-flavors">
+//     <option value="Chocolate">
+//     <option value="Coconut">
+//     <option value="Mint">
+//     <option value="Strawberry">
+//     <option value="Vanilla">
+// </datalist>
--- frontend/modules/render.js
+++ frontend/modules/render.js
@@ -13,9 +13,8 @@ class DOM {
 
   static renderNewGradForm() {
     this.grads.children[0].innerHTML += '<p class="close">close X</p>';
-    this.grads.children[0].innerHTML += this.formBuilder();
+    this.grads.children[0].innerHTML += HTMLBuilder.formBuilder();
     this.toggleForm();
-
   }
 
   static toggleForm() {
@@ -41,6 +40,7 @@ class DOM {
       }
       if (event.target.value == "submit") {
         event.preventDefault();
+        console.log(event.target)
         let newGradObj = DOM.grabValuesForm(
           event.target.previousSibling.children
         );
@@ -117,100 +117,7 @@ class DOM {
     query.value = "";
   }
 
-  static formBuilder() {
-    let html = '<form action="">';
-    let course = [
-      "Software Engineering",
-      "Data Science",
-      "Cybersecurity",
-      "UX/UI Design",
-    ];
-    let campus = [
-      "Austin",
-      "Chicago",
-      "Denver",
-      "Houston",
-      "New York",
-      "San Francisco",
-      "Seattle",
-      "Washington, D.C.",
-      "London",
-      "Online",
-    ];
-
-    let attributes = [
-      "name",
-      "email",
-      "avatar",
-      "cohort",
-      "course",
-      "campus",
-      "company",
-      "street",
-      "city",
-      "postcode",
-      "state",
-      "country",
-      "remote_work",
-    ];
-    html += attributes
-      .map((attr) => {
-        if (attr == "course") {
-          return this.optionsBuilder(course, attr);
-        } else if (attr == "company") {
-          return this.optionsBuilder(ApiAdapter.companies, attr);
-        } else if (attr == "cohort") {
-          return `
-            <div class="float-label">
-              <input type="month" id="cohort" name="cohort"
-                min="2012-01">
-               <label for="cohort">Select you Cohort:</label>
-            </div>
-            `;
-        } else if (attr == "campus") {
-          return this.optionsBuilder(campus, attr);
-        } else if (attr == "remote_work") {
-          return `
-            <div class="float-label">
-              <input type="radio" id="Remote" name='remote_work' value="true">
-              <label for="Remote">Working Remotely</label><br>
-              <input type="radio" id="Office" name='remote_work' value="false" checked>
-              <label for="Office">Office Base</label><br>
-            </div>
-            `;
-        } else {
-          return `   
-       <div class="float-label">
-          <input type="text" name="${attr}" required />
-            <label for="${attr}">${attr}</label>
-        </div>
-            `;
-        }
-      })
-      .join("");
-    html +=
-      '</form><input type="submit" value="submit" class="button__add button__submit">';
-    return html;
-  }
-  static optionsBuilder(collection, attr) {
-    return `     
-    <div class="float-label">     
-    <select name="${attr}" id="${attr}">
-      <option>select you ${attr}</option>
-      ${collection
-        .map((c) => {
-          let option = "";
-          if (typeof c == "object") {
-            option = `<option value="${c.slug}">${c.name}</option>`;
-          } else {
-            option = `<option value="${c}">${c}</option>`;
-          }
-          return option;
-        })
-        .join("")}
-    </select>
-    </div>`;
-  }
+  
 
   static grabValuesForm(nodes) {
     let postObj = {};
@@ -220,18 +127,17 @@ class DOM {
     }
     return postObj;
   }
-  static renderError(grad){
-        document
-          .querySelector(".form-container")
-          .insertAdjacentHTML("afterbegin", HTMLBuilder.errors(grad.message));
-        setTimeout(this.deleteErrors, 5000)
+  static renderError(grad) {
+    document
+      .querySelector(".form-container")
+      .insertAdjacentHTML("afterbegin", HTMLBuilder.errors(grad.message));
+    setTimeout(this.deleteErrors, 5000);
   }
 
-  static deleteErrors(){
-   let errors = document.querySelector(".errors")
-   errors.style.opacity = 0;
-   setTimeout(()=> errors.remove(), 1000)
-
+  static deleteErrors() {
+    let errors = document.querySelector(".errors");
+    errors.style.opacity = 0;
+    setTimeout(() => errors.remove(), 1000);
   }
 }
 
